6c02429c80321cd6b0716d406c48c91e289429e4
AguZhirui <uceszg0@ucl.ac.uk>
2019-03-14 11:20:52 -0400

    develope usertracking file

--- uceszg0/www/js/userTracking.js
+++ uceszg0/www/js/userTracking.js
@@ -1,13 +1,19 @@
+//Global Variable
+var userMarker;
 function trackLocation() {
 if (navigator.geolocation) {
 navigator.geolocation.watchPosition(showPosition);
-} else {
-document.getElementById('showLocation').innerHTML = "Geolocation is not supported by this browser.";
-}
-}
-function showPosition(position) {
-document.getElementById('showLocation').innerHTML = "Latitude: " + position.coords.latitude +
-"<br>Longitude: " + position.coords.longitude;
+alert("Loading Current Location")
+} 
+else {
+document.getElementById('showLocation').innerHTML = "Geolocation is not supported by this browser.";}
 }
 
-L.marker([position.coords.latitude, position.coords.longitude]).addTo(mymap).bindPopup("<b>You were here</b>");
+function showPosition(position) {
+	if (userMarker){
+		mymap.removeLayer(userMarker);
+	}
+	userMarker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(mymap).bindPopup("<b>You were here</b>");
+    mymap.locate({setView: true, maxZoom: 16});
+}
+
